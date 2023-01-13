070b6dfd441956de30f574dc42b72cd330056f61
Einzel <einzel0102@mail.ru>
2023-01-13 19:01:57 +0800

    задача 2 с изменением текста

---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Работа с текстом 
+// Дан текст. В тексте все пробелы нужно заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежеле вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newtext = Replace(text, ' ' , '-');
+Console.WriteLine(newtext);
+Console.WriteLine();
+
+string newnewtext = Replace(newtext, 'к' , 'К');
+Console.WriteLine(newnewtext);
+Console.WriteLine();
+
+string newnewnewtext = Replace(newnewtext, 'C' , 'c');
+Console.WriteLine(newnewnewtext);
